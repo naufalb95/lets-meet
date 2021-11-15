35771377dd2be2c22e96dc1b9868ccd30462fbc8
naufalb95 <naufalfbudiman@gmail.com>
2021-11-15 17:37:22 +0700

    Remove api keys

--- client/src/components/Create.vue
+++ client/src/components/Create.vue
@@ -61,7 +61,7 @@ export default {
   async mounted () {
     navigator.geolocation.getCurrentPosition(this.showPosition)
     const googleMapApi = await GoogleMapsApiLoader({
-      apiKey: 'AIzaSyDOJpP-lEnakyV5PKJHbSOzKxTUu9oIeh0'
+      apiKey: process.env.VUE_APP_GOOGLE_MAPS_API_KEY
     })
 
     this.google = googleMapApi
