d44a93d40361d0e5b4ab90d969e2cd58b2facf5c
naufalb95 <naufalfbudiman@gmail.com>
2021-11-16 18:47:01 +0700

    Vmodel binding create and edit event

--- client/src/views/Edit.vue
+++ client/src/views/Edit.vue
@@ -1,24 +1,24 @@
 <template>
   <div v-on:click.stop class="flex justify-center items-center min-h-screen w-full my-28">
-    <form @submit.prevent="" class="bg-white overflow-y-auto overflow-x-hidden shadow-md rounded px-12 py-12 w-1/2">
+    <form @submit.prevent="submitHandler" class="bg-white overflow-y-auto overflow-x-hidden shadow-md rounded px-12 py-12 w-1/2">
         <h1 class="text-center text-3xl text-blue-800 font-bold mb-12">Edit Event</h1>
         <div class="w-full mb-4">
-          <label class="text-center text-lg font-normal">Event Name</label>
-          <input type="text" class="w-full px-3 py-3 mt-1 rounded text-sm border shadow focus:outline-none" placeholder="Yoga Training"/>
+          <label id="name" class="text-center text-lg font-normal">Event Name</label>
+          <input name="name" v-model="name" type="text" class="w-full px-3 py-3 mt-1 rounded text-sm border shadow focus:outline-none" placeholder="Yoga Training"/>
         </div>
         <div class="mt-1 mb-2 flex">
           <div class="w-3/4 mr-3 mb-4">
-            <label class="text-center text-lg font-normal">Date</label>
-            <input type="date" class="w-full px-3 py-3 mt-1 rounded text-sm border shadow focus:outline-none" />
+            <label id="date" class="text-center text-lg font-normal">Date</label>
+            <input name="date" v-model="date" type="date" class="w-full px-3 py-3 mt-1 rounded text-sm border shadow focus:outline-none" />
           </div>
           <div class="w-3/4 mb-4">
-            <label class="text-center text-lg font-normal">Time</label>
-            <input type="time" value="00:00:00" class="w-full px-3 py-3 mt-1 rounded text-sm border shadow focus:outline-none"/>
+            <label id="time" class="text-center text-lg font-normal">Time</label>
+            <input name="time" v-model="time" type="time" value="00:00:00" class="w-full px-3 py-3 mt-1 rounded text-sm border shadow focus:outline-none"/>
           </div>
         </div>
         <div class="w-full mb-4">
-          <label class="text-center text-lg font-normal">Category</label>
-          <select class="w-full px-3 py-3 mt-1 rounded text-sm border shadow focus:outline-none">
+          <label id="category" class="text-center text-lg font-normal">Category</label>
+          <select name="category" v-model="categoryId" class="w-full px-3 py-3 mt-1 rounded text-sm border shadow focus:outline-none">
             <option>AA</option>
             <option>AA</option>
             <option>AA</option>
@@ -27,24 +27,24 @@
           </select>
         </div>
         <div class="w-full mb-4">
-          <label class="text-center text-lg font-normal">Event Type</label>
-          <select class="w-full px-3 py-3 mt-1 rounded text-sm border shadow focus:outline-none">
+          <label id="event-type" class="text-center text-lg font-normal">Event Type</label>
+          <select name="event-type" v-model="eventType" class="w-full px-3 py-3 mt-1 rounded text-sm border shadow focus:outline-none">
             <option value="offline" selected>Offline</option>
             <option value="online">Online</option>
           </select>
         </div>
         <div class="w-full mb-4">
-          <label class="text-center text-lg font-normal">Maximum Participants</label>
-          <input type="number" class="w-full px-3 py-3 mt-1 rounded text-sm border shadow focus:outline-none" placeholder="50"/>
+          <label id="max-participants" class="text-center text-lg font-normal">Maximum Participants</label>
+          <input name="max-participants" v-model="maxParticipants" type="number" class="w-full px-3 py-3 mt-1 rounded text-sm border shadow focus:outline-none" placeholder="50"/>
         </div>
         <div class="w-full mb-4">
-          <label class="text-center text-lg font-normal">Description</label>
-          <textarea class="overflow-y-auto mb-2 px-2 py-2 rounded text-xs border shadow focus:outline-none block w-full" rows="8" placeholder="Type your event description here"/>
+          <label id="description" class="text-center text-lg font-normal">Description</label>
+          <textarea name="description" v-model="description" class="overflow-y-auto mb-2 px-2 py-2 rounded text-xs border shadow focus:outline-none block w-full" rows="8" placeholder="Type your event description here"/>
         </div>
         <div class="w-full mb-4">
-          <label class="text-center text-lg font-normal">Location</label>
+          <label id="location" class="text-center text-lg font-normal">Location</label>
           <form @submit.prevent="changeLocationHandler">
-            <input type="text" class="w-full px-3 py-3 mt-1 rounded text-sm border shadow focus:outline-none mb-2" v-model="findPlace" placeholder="Grand Indonesia" />
+            <input name="location" type="text" class="w-full px-3 py-3 mt-1 rounded text-sm border shadow focus:outline-none mb-2" v-model="findPlace" placeholder="Grand Indonesia" />
           </form>
           <div id="maps" ref="googleMap" class="w-full border rounded shadow"></div>
         </div>
@@ -63,13 +63,21 @@ export default {
     return {
       google: null,
       map: null,
+      mapContainer: null,
+      marker: null,
+      findPlace: null,
       coords: {
         lat: 0,
         lng: 0
       },
-      mapContainer: null,
-      marker: null,
-      findPlace: null
+      name: '',
+      date: '',
+      time: '',
+      location: '',
+      description: '',
+      maxParticipants: 0,
+      categoryId: 1,
+      eventType: 'offline'
     }
   },
   async mounted () {
@@ -85,6 +93,9 @@ export default {
   },
   methods: {
     ...mapActions(['findGooglePlaces']),
+    submitHandler () {
+      console.log('haii')
+    },
     initializeMap () {
       this.mapContainer = this.$refs.googleMap
     },
