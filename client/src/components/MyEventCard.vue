da8c489a31826ae25cd88179ddcf28dfabec02c9
naufalb95 <naufalfbudiman@gmail.com>
2021-11-17 03:30:55 +0700

    Add image to my event list

--- client/src/components/MyEventCard.vue
+++ client/src/components/MyEventCard.vue
@@ -1,28 +1,33 @@
 <template>
-  <div class="w-5/12 px-10 py-8 bg-white rounded-xl mb-3 shadow-lg border border-white">
-    <div class="w-2/3">
-      <h1 class="text-gray-700 text-md font-medium">{{ dateAndTime }}</h1>
-      <h1 class="text-blue-900 font-semibold text-3xl filter drop-shadow-lg">{{ event.event.name }}</h1>
+  <div class="w-5/12 px-10 py-8 bg-white rounded-xl mb-3 shadow-lg border border-white flex items-center">
+    <div id="image" class="bg-gray-100 shadow-md border border-gray-300 rounded-lg">
+      <img :src="event.event.imgUrl" :alt="event.event.name" class="object-contain w-full h-full"/>
     </div>
-    <div class="mt-6">
-      <button @click.prevent="detailHandler" style="width: 100px" class="text-white py-1 font-semibold border border-blue-500 bg-blue-500 rounded hover:bg-blue-600 mr-2">
-        Detail
-      </button>
-      <button @click.prevent="joinMeetHandler" v-if="(isAttending || isHost) && isStart && !isDone" style="width: 100px" class="text-white py-1 font-semibold border border-blue-500 bg-blue-500 rounded hover:bg-blue-600 mr-2">
-        Join Meet
-      </button>
-      <button @click.prevent="leaveEventHandler" v-if="!isHost && isAttending && !isStart && !isDone" style="width: 100px" class="text-red-500 py-1 font-semibold border border-red-500 bg-white rounded hover:bg-red-500 hover:text-white mr-2">
-        Leave
-      </button>
-      <button @click.prevent="editEventHandler" v-if="isHost && !isStart && !isDone" style="width: 100px" class="text-white py-1 font-semibold border border-yellow-500 bg-yellow-500 rounded hover:bg-yellow-600 mr-2">
-        Edit
-      </button>
-      <button @click.prevent="doneEventHandler" v-if="isHost && isStart && !isDone" style="width: 100px" class="text-red-500 py-1 font-semibold border border-red-500 bg-white rounded hover:bg-red-500 hover:text-white mr-2">
-        End Event
-      </button>
-      <button @click.prevent="deleteEventHandler" v-if="isHost && !isStart && !isDone" style="width: 100px" class="text-red-500 py-1 font-semibold border border-red-500 bg-white rounded hover:bg-red-500 hover:text-white mr-2">
-        Delete
-      </button>
+    <div class="pl-4">
+      <div class="w-2/3">
+        <h1 class="text-gray-700 text-md font-medium">{{ dateAndTime }}</h1>
+        <h1 class="text-blue-900 font-semibold text-3xl filter drop-shadow-lg">{{ event.event.name }}</h1>
+      </div>
+      <div class="mt-6">
+        <button @click.prevent="detailHandler" style="width: 100px" class="text-white py-1 font-semibold border border-blue-500 bg-blue-500 rounded hover:bg-blue-600 mr-2">
+          Detail
+        </button>
+        <button @click.prevent="joinMeetHandler" v-if="(isAttending || isHost) && isStart && !isDone" style="width: 100px" class="text-white py-1 font-semibold border border-blue-500 bg-blue-500 rounded hover:bg-blue-600 mr-2">
+          Join Meet
+        </button>
+        <button @click.prevent="leaveEventHandler" v-if="!isHost && isAttending && !isStart && !isDone" style="width: 100px" class="text-red-500 py-1 font-semibold border border-red-500 bg-white rounded hover:bg-red-500 hover:text-white mr-2">
+          Leave
+        </button>
+        <button @click.prevent="editEventHandler" v-if="isHost && !isStart && !isDone" style="width: 100px" class="text-white py-1 font-semibold border border-yellow-500 bg-yellow-500 rounded hover:bg-yellow-600 mr-2">
+          Edit
+        </button>
+        <button @click.prevent="doneEventHandler" v-if="isHost && isStart && !isDone" style="width: 100px" class="text-red-500 py-1 font-semibold border border-red-500 bg-white rounded hover:bg-red-500 hover:text-white mr-2">
+          End Event
+        </button>
+        <button @click.prevent="deleteEventHandler" v-if="isHost && !isStart && !isDone" style="width: 100px" class="text-red-500 py-1 font-semibold border border-red-500 bg-white rounded hover:bg-red-500 hover:text-white mr-2">
+          Delete
+        </button>
+      </div>
     </div>
   </div>
 </template>
@@ -89,3 +94,10 @@ export default {
   }
 }
 </script>
+
+<style scoped>
+  #image {
+    height: 120px;
+    width: 120px;
+  }
+</style>
