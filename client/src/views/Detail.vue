ff1610eeadc7250218b90201713de6c8ce914264
djiauwchaochong <farandi.963@gmail.com>
2021-11-17 23:37:40 +0700

    modal clear

--- client/src/views/Detail.vue
+++ client/src/views/Detail.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="flex mt-16 flex-col">
+  <div class="flex mt-16 flex-col mb-96">
     <div class="text-center" v-if="isLoading">
       <h1>Loading...</h1>
     </div>
@@ -118,6 +118,26 @@ export default {
     },
     attendees () {
       return this.eventDetail.participants?.length + 1
+    },
+    leaveEvent () {
+      return this.$store.state.leaveEvent
+    },
+    deletezEvent () {
+      return this.$store.state.deleteEvent
+    }
+  },
+  watch: {
+    leaveEvent: async function (newVal, oldVal) {
+      await this.userLeaveEvent(this.$route.params.id)
+      this.isAttending = false
+      await this.$store.commit('SET_LEAVE_EVENT', false)
+      await this.$store.commit('SET_IS_MODAL_SHOW_LEAVE', false)
+    },
+    deletezEvent: async function (newVal, oldVal) {
+      await this.deleteEvent(this.$route.params.id)
+      await this.$store.commit('SET_DELETE_EVENT', false)
+      await this.$store.commit('SET_IS_MODAL_SHOW_DELETE', false)
+      this.$router.push({ name: 'MyEvent' })
     }
   },
   methods: {
@@ -149,14 +169,16 @@ export default {
       }
     },
     async leaveEventHandler () {
-      await this.userLeaveEvent(this.$route.params.id)
+      await this.$store.commit('SET_IS_MODAL_SHOW_LEAVE', true)
+      // await this.userLeaveEvent(this.$route.params.id)
 
-      this.isAttending = false
+      // this.isAttending = false
     },
     async deleteEventHandler () {
-      await this.deleteEvent(this.$route.params.id)
+      this.$store.commit('SET_IS_MODAL_SHOW_DELETE', true)
+      // await this.deleteEvent(this.$route.params.id)
 
-      this.$router.push({ name: 'MyEvent' })
+      // this.$router.push({ name: 'MyEvent' })
     },
     async doneEventHandler () {
       await this.doneEvent(this.$route.params.id)
