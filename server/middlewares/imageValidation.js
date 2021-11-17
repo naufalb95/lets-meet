4f2de0db808604ba98cce8e58c69ec11f415444b
giovannyptr <giovannyputri@ymail.com>
2021-11-17 16:10:06 +0700

    fix bugs

---
+++ server/middlewares/imageValidation.js
@@ -0,0 +1,22 @@
+const imageValidation = async (req, res, next) => {
+    try {
+        if (req.file.size > 255000) {
+          throw ({ name: `fileTooBig` })
+        }
+
+        if (
+          req.file.mimetype !== "image/png" &&
+          req.file.mimetype !== "image/jpg" &&
+          req.file.mimetype !== "image/jpeg" 
+        ) {
+          throw ({ name: `wrongFormat` })
+        }
+
+        next()
+    } catch (error) {
+        next(error)
+    }
+}
+
+
+module.exports = imageValidation
